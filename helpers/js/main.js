6d7688ccc59122fe778bad29d750a870af7c12fa
williamwendt31 <williamwendt31@yahoo.com>
2019-02-10 15:02:03 -0800

    finished scrollspy and all onto one page

---
+++ helpers/js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('.about').scrollspy({ target: '#about-us' });
+    $('.serv').scrollspy({ target: '#services' });
+    $('.cont').scrollspy({ target: '#contact' });
+});
